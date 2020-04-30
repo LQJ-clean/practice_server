9e5ce70e9cfdd6ef43a97bc98673cf4875f2ae19
cleaning <2056910100@qq.com>
2020-04-30 18:01:02 +0800

    init

--- demo/README.md
+++
@@ -1,2 +0,0 @@
-# practice_server
-practice
--- demo/src/main/java/com/example/demo/vip/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.vip;
-
-import com.example.demo.core.GeneralOperation;
-
-public interface UserService extends GeneralOperation<User> {
-
-}
